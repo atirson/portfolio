a661dcbf8b46aa16c0a13c20da82a89e58d13aa1
atirson <fabiano.oliveira2213@gmail.com>
2022-01-09 00:21:23 -0300

    add: Created a section Footer

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next'
 import About from './components/About'
 import Contact from './components/Contact'
+import Footer from './components/Footer'
 import Header from './components/Header'
 import Skills from './components/Skills'
 import Timeline from './components/Timeline'
@@ -13,6 +14,7 @@ const Home: NextPage = () => {
       <Skills />
       <Timeline />
       <Contact />
+      <Footer />
     </>
   )
 }
